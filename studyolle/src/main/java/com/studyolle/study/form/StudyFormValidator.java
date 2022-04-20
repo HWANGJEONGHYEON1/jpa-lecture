b0147da1897ddb10108f6852429dacc3fe5e164a
황정현 <jhhwang@wemakeprice.com>
2022-04-20 16:18:55 +0900

    [study] 스터디 조회 개설 소개 배너

---
+++ studyolle/src/main/java/com/studyolle/study/form/StudyFormValidator.java
@@ -0,0 +1,2 @@
+package com.studyolle.study.form;public class StudyFormValidator {
+}
---
+++ studyolle/src/main/java/com/studyolle/study/validator/StudyDescriptionForm.java
@@ -0,0 +1,9 @@
+package com.studyolle.study;
+
+import lombok.Data;
+
+@Data
+public class StudyDescriptionForm {
+    private String shortDescription;
+    private String fullDescription;
+}
